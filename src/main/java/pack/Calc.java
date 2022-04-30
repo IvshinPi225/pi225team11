27295698fa757c22d4bd4778ec3b80bb5b8046d5
Andrey <fahanane@mail.ru>
2022-04-30 18:54:48 +0500

    Ivshin change jakaarta to javax

--- src/main/java/pack/Calc.java
+++ src/main/java/pack/Calc.java
@@ -2,9 +2,9 @@ package pack;
 
 import java.io.IOException;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
 @WebServlet(name="Calc", urlPatterns="/Calc")
 public class Calc extends HttpServlet {
